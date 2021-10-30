bc1b6858522257e0168d66d7cbba72186c34acce
Ivanna <iva.63@hotmail.com>
2021-10-30 16:25:44 +0200

    Update Direction.cpp

--- RayTracer/Direction.cpp
+++ RayTracer/Direction.cpp
@@ -1,11 +1,20 @@
 #include "Direction.h"
 
 Direction Direction::crossProduct(Direction inD) {
-	this->x = this->x * inD.x;
+	
+	Direction crossP;
+	/*this->x = this->x * inD.x;
 	this->y = this->y * inD.y;
-	this->z = this->z * inD.z;
+	this->z = this->z * inD.z;*/
 
-	return *this;
+	/*cross_P[0] = vect_A[1] * vect_B[2] - vect_A[2] * vect_B[1];
+	cross_P[1] = vect_A[2] * vect_B[0] - vect_A[0] * vect_B[2];
+	cross_P[2] = vect_A[0] * vect_B[1] - vect_A[1] * vect_B[0];*/
+	crossP.x = this->y * inD.z - this->z * inD.y;
+	crossP.y = this->z * inD.x - this->x * inD.z;
+	crossP.z = this->x * inD.y - this->y * inD.x;
+
+	return crossP;
 }
 
 double Direction::dotProduct(Direction inD) {
